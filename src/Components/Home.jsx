1faabc875f41b071e065ff3153424d8d33f39203
Collins Asante <mr.asanteeprog@gmail.com>
2025-06-16 19:34:38 +0000

    Revised Fonts and Colors

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,12 +8,15 @@ const Home = () => {
         <div className="row align-items-center mt-5 mt-lg-0">
           <div className="col-lg-5">
             <div className="home-heading">
-              <h6 className="text-uppercase text-muted">
-                I'm Graphics Designer
+              <h6 className="lh-sm">
+                <span className="text-primary">
+                  {" "}
+                  this is your exclusive space{" "}
+                </span>
               </h6>
               <h1 className="lh-sm">
-                Make your life simplier with{" "}
-                <span className="text-primary">Kasy</span>
+                A Central Resource For Everything You Need to{" "}
+                <span className="text-primary">Thrive</span>
               </h1>
             </div>
           </div>
